c8c10c66abfb06be932232f22e5dbc90c057460d
Ian Ibbotson <ianibbo@googlemail.com>
2021-05-05 13:15:23 +0100

    Working on migrations and domain model

--- service/grails-app/migrations/create-mod-remote-sync.groovy
+++ service/grails-app/migrations/create-mod-remote-sync.groovy
@@ -1,5 +1,31 @@
 databaseChangeLog = {
-    changeSet(author: "ianibbo (manual)", id: "051220201808-001") {
-        createSequence(sequenceName: "hibernate_sequence")
+  changeSet(author: "ianibbo (manual)", id: "i202105051310-001") {
+    createSequence(sequenceName: "hibernate_sequence")
+  }
+
+  changeSet(author: "ianibbo (generated)", id: "i202105051311-001") {
+
+    createTable(tableName: "mrs_authority") {
+      column(name: "aut_id", type: "VARCHAR(36)") {
+        constraints(nullable: "false")
+      }
+
+      column(name: "aut_version", type: "BIGINT") {
+        constraints(nullable: "false")
+      }
+
+      column(name: "aut_date_created", type: "TIMESTAMP") {
+        constraints(nullable: "true")
+      }
+
+      column(name: "aut_date_updated", type: "TIMESTAMP") {
+        constraints(nullable: "true")
+      }
+
+      column(name: "aut_name", type: "VARCHAR(128)") {
+        constraints(nullable: "false")
+      }
     }
+  }
+
 }
