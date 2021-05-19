b40e4a848b33cb1e7de209a7b49e60def1a8d85b
Ian Ibbotson <ianibbo@gmail.com>
2021-05-19 13:59:26 +0100

    Add hmac helper

--- testdata/source_scripts/test1.groovy
+++ testdata/source_scripts/test1.groovy
@@ -1,7 +1,20 @@
 import mod_remote_sync.source.RemoteSyncActivity;
 import mod_remote_sync.source.RecordSourceController;
+import javax.crypto.spec.SecretKeySpec
+import javax.crypto.Mac
+import org.apache.commons.codec.binary.Hex
 
 public class RemoteSyncOne implements RemoteSyncActivity {
+
+  private String makeAuth(String path, String timestamp, String nonce, String q) {
+    String string_to_hash = "GET${path}${timestamp}${nonce}${q}".toString()
+
+    Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+    SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256");
+    sha256_HMAC.init(secret_key);
+    return Hex.encodeHexString(sha256_HMAC.doFinal(string_to_hash.getBytes("UTF-8")));
+  }
+
 
   public void getLatestUpdates() {
     println("Test::getLatestUpdates");
