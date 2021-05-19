b8425c1f97c5e3b15c97b46d8c1b04291f9c2ec6
Ian Ibbotson <ianibbo@gmail.com>
2021-05-19 10:21:00 +0100

    Corrected transform plugins

--- testdata/source_scripts/ProcessLaserLicense.groovy
+++ testdata/source_scripts/ProcessLaserLicense.groovy
@@ -1,5 +1,13 @@
 import mod_remote_sync.source.TransformProcess;
 
-public class ProcessLaserSubscription implements TransformProcess {
+public class ProcessLaserLicense implements TransformProcess {
+
+  public Map preflightCheck(Map input_record) {
+    return [:]
+  }
+
+  public Map process(Map input_record) {
+    return [:]
+  }
 
 }
