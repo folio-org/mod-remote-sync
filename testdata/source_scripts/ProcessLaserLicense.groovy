47c5176dbfba89661c234817d6bc6a3ee7ca912d
Ian Ibbotson <ianibbo@gmail.com>
2021-06-11 18:16:14 +0100

    updated

--- testdata/source_scripts/ProcessLaserLicense.groovy
+++ testdata/source_scripts/ProcessLaserLicense.groovy
@@ -10,6 +10,15 @@ public class ProcessLaserLicense implements TransformProcess {
                             ApplicationContext ctx,
                             Map local_context) {
     log.debug("ProcessLaserLicense::preflightCheck()");
+    Map result = [
+      preflightStatus:'FAIL'
+    ]
+
+    // We don't create licenses unless a user has told us to create a new one, or which license we should map
+    // to. If we don't know - feedback that we need to know which license to map to, to create new, or not to map
+    // If we know, the process can continue to process
+
+    // manualCheckOnCreateResource()
     return [:]
   }
 
