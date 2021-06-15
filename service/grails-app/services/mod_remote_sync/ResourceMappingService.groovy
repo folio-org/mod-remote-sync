ca20289bcd156c8eb8785fe535b01af8e9c63192
Ian Ibbotson <ianibbo@gmail.com>
2021-06-15 10:30:34 +0100

    Improve resource mapping

--- service/grails-app/services/mod_remote_sync/ResourceMappingService.groovy
+++ service/grails-app/services/mod_remote_sync/ResourceMappingService.groovy
@@ -10,10 +10,33 @@ import com.k_int.okapi.OkapiTenantResolver
 @Transactional
 class ResourceMappingService {
 
-  public Map lookupMapping(String source,
-                           String source_id,
-                           String mapping_context) {
-    return [:]
+  private static final String FIND_MAPPING = '''
+select rm 
+from ResourceMapping as rm
+where rm.source = :source
+and rm.sourceId = :id
+and rm.mappingContext = ctx
+'''
+
+  public ResourceMapping lookupMapping(String source,
+                                       String source_id,
+                                       String mapping_context) {
+
+    ResourceMapping result = null;
+
+    List<ResourceMapping> rmq = ResourceMapping.executeQuery(FIND_MAPPING,[source: source, id:source_id, ctx:mapping_context]);
+
+    switch ( rmq.size() ) {
+      case 0:
+        break;
+      case 1:
+        result = rmq[0]
+        break;
+      default:
+        break;
+    }
+
+    return result
   }
 
 }
