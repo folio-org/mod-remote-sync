6886919c50748f5e4dec14555a5d516348f11fe9
Ian Ibbotson <ianibbo@gmail.com>
2021-09-01 15:41:41 +0100

    Working on stand alone FOLIO client

--- service/src/main/groovy/mod_remote_sync/folio/FolioClientImpl.groovy
+++ service/src/main/groovy/mod_remote_sync/folio/FolioClientImpl.groovy
@@ -7,11 +7,16 @@ import com.k_int.web.toolkit.settings.AppSetting
 import com.k_int.web.toolkit.refdata.*
 import com.k_int.okapi.OkapiTenantResolver
 import grails.converters.JSON
-import com.k_int.okapi.OkapiClient
 import groovy.util.logging.Slf4j
 import grails.core.GrailsApplication
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.context.request.RequestContextHolder
+
+import groovyx.net.http.HttpBuilder
+import groovyx.net.http.FromServer
+import groovy.json.JsonOutput
+import static groovyx.net.http.HttpBuilder.configure
+
 
 /**
  * Folio helper service impl
@@ -21,29 +26,129 @@ import org.springframework.web.context.request.RequestContextHolder
 @Transactional
 class FolioClientImpl implements FolioClient {
 
-  OkapiClient okapiClient = null;
+  private String okapi_host;
+  private String okapi_port;
+  private String url;
+  private String tenant;
+  private String user;
+  private String pass;  
+  private Map session_ctx = [:]
 
-  public Object okapiPost(String path, Object o, Map params=null) {
-    log.debug("FolioHelperService::okapiPost(${path},....)");
-    log.debug("Request attributes: ${RequestContextHolder.requestAttributes}");
-    def result = okapiClient.post(path, o, params)
-    log.debug("Result of okapiPost(${path}...): ${result}");
-    return result;
+
+  public FolioClientImpl(String okapi_host,
+                         String okapi_port,
+                         String tenant,
+                         String user,
+                         String pass) {
+    this.okapi_host = okapi_host;
+    this.okapi_port = okapi_port;
+    this.tenant = tenant;
+    this.user = user;
+    this.pass = pass;
+    this.url = "http://${okapi_host}:${oakpi_port}".toString()
   }
+
+  public boolean init() {
+  }
+
+  private login() {
+
+    def postBody = [username: this.user, password: this.pass]
+
+    println("attempt login (url=http://${url}) / tenant=${tenant}) ${postBody}");
+
+    def http = configure {
+      request.uri = url;
+    }
+
+    def result = http.post {
+      request.uri.path='/bl-users/login'
+      request.headers['X-Okapi-Tenant']=this.tenant;
+      request.headers['accept']='application/json'
+
+      request.uri.query=[expandPermissions:false,fullPermissions:false]
+      request.contentType='application/json'
+      request.body=postBody
+
+      response.failure{ FromServer fs, Object body ->
+        println("Problem logging into FOLIO ${new String(body)}");
+      }
+
+      response.success{ FromServer fs, Object body ->
+        session_ctx.auth = body
+        println("OK _ headers == ${fs.getHeaders()}");
+        session_ctx.token = fs.getHeaders().find { it.key.equalsIgnoreCase('x-okapi-token')}?.value
+        println("LOGIN OK - TokenHeader=${session_ctx.token}");
+      }
+
+    }
+  }
+
+  def ensureLogin() {
+    if ( session_ctx.auth == null )
+      login()
+  }
+
+
+  // public Object okapiPost(String path, Object o, Map params=null) {
+  //   log.debug("FolioClientImpl::okapiPost(${path},....)");
+  //   log.debug("Request attributes: ${RequestContextHolder.requestAttributes}");
+    //def result = okapiClient.post(path, o, params)
+  //   log.debug("Result of okapiPost(${path}...): ${result}");
+  //   return result;
+  // }
   
   public Object okapiPut(String path, Object o, Map params=null) {
-    log.debug("FolioHelperService::okapiPut(${path},....)");
-    def result = okapiClient.put(path, o, params)
+    log.debug("FolioClientImpl::okapiPut(${path},....)");
+    //def result = okapiClient.put(path, o, params)
     log.debug("Result of okapiPut(${path}...): ${result}");
     return result;
   }
 
   public Object okapiGet(String path, Map params) {
-    log.debug("FolioHelperService::okapiGet(${path},${params}....)");
-    def result = okapiClient.get(path, params)
+    log.debug("FolioClientImpl::okapiGet(${path},${params}....)");
+    //def result = okapiClient.get(path, params)
     log.debug("Result of okapiGet(${path}...): ${result}");
     return result;
   }
+
+  public Object okapiPost(String path, Object o, Map params=null) {
+
+    log.debug("FolioClientImpl::okapiPost(${path},....)");
+
+    Object result = null;
+
+    ensureLogin();
+
+    // lookup
+    def http = configure {
+      request.uri = url
+    }
+
+    http.post {
+      request.uri.path = path;
+      request.headers['X-Okapi-Tenant']=this.tenant;
+      request.headers['accept']='application/json'
+      request.headers['X-Okapi-Token']=session_ctx.token
+      request.contentType='application/json'
+      request.body = params
+      response.failure{ FromServer fs, Object body ->
+        println("Problem in term creation: ${new String(body)}");
+      }
+
+      response.success{ FromServer fs, Object body ->
+        println("Term POST OK");
+        result = body;
+      }
+
+    }
+
+    //def result = okapiClient.post(path, o, params)
+    log.debug("Result of okapiPost(${path}...): ${result}");
+    return result;
+  }
+
+
 
   // See https://gitlab.com/knowledge-integration/folio/middleware/folio-laser-erm-legacy/-/blob/master/spike/process.groovy#L207
   // See https://gitlab.com/knowledge-integration/folio/middleware/folio-laser-erm-legacy/-/blob/master/spike/FolioClient.groovy#L74
