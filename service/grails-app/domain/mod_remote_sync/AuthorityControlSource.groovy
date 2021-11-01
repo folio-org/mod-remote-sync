c3deaa5e08b5409c636efb579a2a4cddddf813b3
Ian Ibbotson <ianibbo@gmail.com>
2021-11-01 10:34:12 +0000

    Tidy up authority control source and add label

--- service/grails-app/domain/mod_remote_sync/AuthorityControlSource.groovy
+++ service/grails-app/domain/mod_remote_sync/AuthorityControlSource.groovy
@@ -8,24 +8,27 @@ public class AuthorityControlSource implements MultiTenant<AuthorityControlSourc
   String vocabUri
   String vocabType
   String serviceUrl
+  String label
   Date dateCreated
   Date lastUpdated
 
   static constraints = {
-      vocabUri (nullable : false, blank: false, unique: true)
-     vocabType (nullable : false, blank: false, unique: true)
-    serviceUrl (nullable : false, blank: false, unique: true)
+      vocabUri (nullable : true, blank: false)
+     vocabType (nullable : true, blank: false)
+    serviceUrl (nullable : true, blank: false)
+         label (nullable : true, blank: false)
   }
 
   static mapping = {
-    table 'mrs_authority_control_source'
-    id                     column : 'acs_id', generator: 'uuid2', length:36
-    version                column : 'acs_version'
-    dateCreated            column : 'acs_date_created'
-    lastUpdated            column : 'acs_date_updated'
-    vocabUri               column : 'acs_vocab_uri'
-    vocabType              column : 'acs_vocab_type'
-    serviceUrl             column : 'acs_service_url'
+    table 'mrs_authority_control_src'
+             id column : 'acs_id', generator: 'uuid2', length:36
+        version column : 'acs_version'
+    dateCreated column : 'acs_date_created'
+    lastUpdated column : 'acs_date_updated'
+       vocabUri column : 'acs_vocab_uri'
+      vocabType column : 'acs_vocab_type'
+     serviceUrl column : 'acs_service_url'
+          label column : 'acs_label'
   }
 
 }
