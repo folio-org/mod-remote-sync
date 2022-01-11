66ad43847feb1ee544b18ea8aff824ff8d1fd9be
Ian Ibbotson <ianibbo@gmail.com>
2022-01-11 11:07:27 +0000

    updated

---
+++ service/grails-app/domain/mod_remote_sync/CodeSigningAuthority.groovy
@@ -0,0 +1,29 @@
+package mod_remote_sync
+
+import grails.gorm.MultiTenant;
+
+/**
+ */
+public class CodeSigningAuthority  implements MultiTenant<CodeSigningAuthority> {
+
+  String id
+  String name
+  String publicKey
+
+  static constraints = {
+         name (nullable : false)
+    publickey (nullable : true)
+  }
+
+  static mapping = {
+    table 'mrs_code_signing_authority'
+    id                     column : 'csa_id', generator: 'uuid2', length:36
+    version                column : 'csa_version'
+    name                   column : 'csa_name'
+    publicKey              column : 'csa_public_key'
+  }
+
+  public String toString() {
+    return "CodeSigningAuthority::id:${id}/name:${name}".toString()
+  }
+}
